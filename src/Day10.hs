05c8cb11777d3a83df8d988e695b013196a4744b
Chad Groft <clgroft@gmail.com>
2020-12-10 09:12:46 -0500

    Complete day 10

--- src/Day10.hs
+++ src/Day10.hs
@@ -4,5 +4,31 @@ module Day10
   ( day10
   ) where
 
+import           Data.List                      ( sort )
+
 day10 :: String -> String
-day10 _ = "day10\n"
+day10 input =
+  "Product of 1-diff and 3-diff: "
+    ++ (show $ productOneThree adapters)
+    ++ "\n"
+    ++ "Number of arrangements: "
+    ++ (show . snd . head $ allArrangements adapters)
+    ++ "\n"
+  where adapters = allAdapters input
+
+type Adapter = Int
+
+allAdapters :: String -> [Adapter]
+allAdapters = (0 :) . sort . map read . lines
+
+count :: (a -> Bool) -> [a] -> Int
+count p = length . filter p
+
+productOneThree :: [Adapter] -> Int
+productOneThree adapters =
+  count (== 1) differences * (1 + count (== 3) differences)
+  where differences = zipWith (-) (tail adapters) adapters
+
+allArrangements :: [Adapter] -> [(Adapter, Int)]
+allArrangements adapters = foldr f [(last adapters + 3, 1)] adapters
+  where f a ps = (a, sum . map snd $ takeWhile ((<= a + 3) . fst) ps) : ps
